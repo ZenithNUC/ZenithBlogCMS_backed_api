dfeb628118e7c3e92dcb3d362d2611ae5c577b8d
ZenithNUC <lizengzhi1998@foxmail.com>
2021-05-23 21:52:40 +0800

    add:增加了列出管理员列表的功能

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,70 @@ let exportObj = {
 module.exports = exportObj
 
 function list(req,res){
+    const resObj = Common.clone (Constant.DEFAULT_SUCCESS);
+    // 定义一个async任务
+    let tasks = {
+        // 校验参数方法
+        checkParams: (cb) => {
+            // 调用公共方法中的校验参数方法，成功继续后面操作，失败则传递错误信息到async最终方法
+            Common.checkParams (req.query, ['page', 'rows'], cb);
+        },
+        // 查询方法，依赖校验参数方法
+        query: ['checkParams', (results, cb) => {
+            // 根据前端提交参数计算SQL语句中需要的offset，即从多少条开始查询
+            let offset = req.query.rows * (req.query.page - 1) || 0;
+            // 根据前端提交参数计算SQL语句中需要的limit，即查询多少条
+            let limit = parseInt (req.query.rows) || 20;
+            // 设定一个查询条件对象
+            let whereCondition = {};
+            // 如果查询用户名存在，查询对象增加用户名
+            if(req.query.username){
+                whereCondition.username = req.query.username;
+            }
+            // 通过offset和limit使用admin的model去数据库中查询，并按照创建时间排序
+            AdminModel
+                .findAndCountAll ({
+                    where: whereCondition,
+                    offset: offset,
+                    limit: limit,
+                    order: [['created_at', 'DESC']],
+                })
+                .then (function (result) {
+                    // 查询结果处理
+                    // 定义一个空数组list，用来存放最终结果
+                    let list = [];
+                    // 遍历SQL查询出来的结果，处理后装入list
+                    result.rows.forEach ((v, i) => {
+                        let obj = {
+                            id: v.id,
+                            username: v.username,
+                            name: v.name,
+                            role: v.role,
+                            lastLoginAt: dateFormat (v.lastLoginAt, 'yyyy-mm-dd HH:MM:ss'),
+                            createdAt: dateFormat (v.createdAt, 'yyyy-mm-dd HH:MM:ss')
+                        };
+                        list.push (obj);
+                    });
+                    // 给返回结果赋值，包括列表和总条数
+                    resObj.data = {
+                        list,
+                        count: result.count
+                    };
+                    // 继续后续操作
+                    cb (null);
+                })
+                .catch (function (err) {
+                    // 错误处理
+                    // 打印错误日志
+                    console.log (err);
+                    // 传递错误信息到async最终方法
+                    cb (Constant.DEFAULT_ERROR);
+                });
 
+        }]
+    };
+    // 执行公共方法中的autoFn方法，返回数据
+    Common.autoFn (tasks, res, resObj)
 }
 
 function info(req,res){
