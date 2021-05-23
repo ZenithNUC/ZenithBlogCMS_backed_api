bfd860d80db6785e5207961569ebf1ce8f07c04d
ZenithNUC <lizengzhi1998@foxmail.com>
2021-05-23 21:26:40 +0800

    add:增加了博客介绍的功能

--- controllers/info.js
+++ controllers/info.js
@@ -15,9 +15,45 @@ let exportObj = {
 module.exports = exportObj;
 
 function info (req, res) {
+    const resObj = Common.clone (Constant.DEFAULT_SUCCESS);
+    let tasks = {
+        query: cb => {
+            // 使用info的model中的方法查询
+            InfoModel
+                // 查询指定id为1的数据
+                .findByPk (1)
+                .then (function (result) {
+                    // 查询结果处理
+                    // 如果查询到结果
+                    if(result){
+                        // 将查询到的结果给返回对象赋值
+                        resObj.data = {
+                            id: result.id,
+                            title: result.title,
+                            subtitle: result.subtitle,
+                            about: result.about,
+                            createdAt: dateFormat (result.createdAt, 'yyyy-mm-dd HH:MM:ss')
+                        };
+                        // 继续后续操作
+                        cb(null);
+                    }else{
+                        // 查询失败，传递错误信息到async最终方法
+                        cb (Constant.BLOG_INFO_NOT_EXSIT);
+                    }
+                })
+                .catch (function (err) {
+                    // 错误处理
+                    // 打印错误日志
+                    console.log (err);
+                    // 传递错误信息到async最终方法
+                    cb (Constant.DEFAULT_ERROR);
+                });
 
+        }
+    }
+    Common.autoFn(tasks,res,resObj)
 }
 
 function update (req, res) {
-
+    
 }
